dc2a1688f2f17c9de28fc8ddac9253726dccedff
kunkka19xx <nobita079x@gmail.com>
2025-02-06 14:03:52 +0900

    ok tmux

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -2,7 +2,14 @@
 
 {
   programs.tmux = {
-    plugins = [ pkgs.tmuxPlugins.vim-tmux-navigator ];
+    # need cargo for some plugins???
+    plugins = [
+      pkgs.tmuxPlugins.vim-tmux-navigator
+      pkgs.tmuxPlugins.catppuccin
+      # pkgs.tmuxPlugins.sensible
+      pkgs.tmuxPlugins.weather
+      pkgs.tmuxPlugins.cpu
+    ];
     enable = true;
     extraConfig = builtins.readFile ../../dotfiles/tmux/tmux.conf;
   };
