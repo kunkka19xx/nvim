cd65b6090c4610657b5e3586375e20992021b4c2
kunkka19xx <nobita079x@gmail.com>
2025-02-14 19:45:22 +0900

    map key as mac os

--- nixos/surface-pro-7/configuration.nix
+++ nixos/surface-pro-7/configuration.nix
@@ -138,22 +138,22 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.interception-tools =
+  /* services.interception-tools =
     let
-      itools = pkgs.interception-tools;
-      itools-caps = pkgs.interception-tools-plugins.caps2esc;
+    itools = pkgs.interception-tools;
+    itools-caps = pkgs.interception-tools-plugins.caps2esc;
     in
     {
-      enable = true;
-      plugins = [ itools-caps ];
-      # requires explicit paths: https://github.com/NixOS/nixpkgs/issues/126681
-      udevmonConfig = pkgs.lib.mkDefault ''
-        - JOB: "${itools}/bin/intercept -g $DEVNODE | ${itools-caps}/bin/caps2esc -m 1 | ${itools}/bin/uinput -d $DEVNODE"
-          DEVICE:
-            EVENTS:
-              EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
-      '';
-    };
+    enable = true;
+    plugins = [ itools-caps ];
+    # requires explicit paths: https://github.com/NixOS/nixpkgs/issues/126681
+    udevmonConfig = pkgs.lib.mkDefault ''
+    - JOB: "${itools}/bin/intercept -g $DEVNODE | ${itools-caps}/bin/caps2esc -m 1 | ${itools}/bin/uinput -d $DEVNODE"
+    DEVICE:
+    EVENTS:
+    EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+    }; */
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
